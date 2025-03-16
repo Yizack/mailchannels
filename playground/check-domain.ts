ad587809ae32cdc66fe7a3864ace777a3d7dfceb
Yizack Rangel <yizackr@gmail.com>
2025-03-16 16:20:49 -0500

    chore(playground): add check domain

---
+++ playground/check-domain.ts
@@ -0,0 +1,24 @@
+import { MailChannels } from "../src/mailchannels";
+
+process.loadEnvFile();
+
+const {
+  MAILCHANNELS_API_KEY: apiKey,
+  MAILCHANNELS_DKIM_DOMAIN: dkimDomain,
+  MAILCHANNELS_DKIM_SELECTOR: dkimSelector,
+  MAILCHANNELS_DKIM_PRIVATE_KEY: dkimPrivateKey,
+  MAILCHANNELS_SENDER_ID: senderId
+} = process.env as Record<string, string>;
+
+const mailchannels = new MailChannels(apiKey);
+const { results } = await mailchannels.emails.checkDomain({
+  domain: dkimDomain,
+  dkim: {
+    domain: dkimDomain,
+    selector: dkimSelector,
+    privateKey: dkimPrivateKey
+  },
+  senderId: senderId
+});
+
+console.info(JSON.stringify(results, null, 2));
