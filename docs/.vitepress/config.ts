f7c46ef0c01f31fd2ec866202ced54b001182511
Yizack Rangel <yizackr@gmail.com>
2025-03-17 17:13:00 -0500

    chore(docs): sidebar config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vitepress";
 import { groupIconMdPlugin, groupIconVitePlugin } from "vitepress-plugin-group-icons";
+import sidebarConfig from "./theme/sidebar";
 
 export default defineConfig({
   title: "@yizack/mailchannels",
@@ -24,38 +25,7 @@ export default defineConfig({
       { text: "Modules", link: "/modules" },
       { text: "Examples", link: "/examples" }
     ],
-    sidebar: [
-      {
-        text: "Getting Started",
-        link: "/guide"
-      },
-      {
-        text: "Email API",
-        collapsed: false,
-        items: [
-          { text: "Send", link: "/modules/emails/send" },
-          { text: "Check Domain", link: "/modules/emails/check-domain" },
-          {
-            text: "Webhooks",
-            collapsed: true,
-            items: [
-              { text: "Enroll for Webhook", link: "/modules/emails/webhooks/enroll" },
-              { text: "Get Webhooks", link: "/modules/emails/webhooks/get-all" },
-              { text: "Delete Webhooks", link: "/modules/emails/webhooks/delete" },
-              { text: "Get Signing Key", link: "/modules/emails/webhooks/signing-key" }
-            ]
-          }
-        ]
-      },
-      {
-        text: "Examples",
-        collapsed: false,
-        items: [
-          { text: "Markdown Examples", link: "/markdown-examples" },
-          { text: "Runtime API Examples", link: "/api-examples" }
-        ]
-      }
-    ],
+    sidebar: sidebarConfig,
     socialLinks: [
       { icon: "github", link: "https://github.com/Yizack/mailchannels" }
     ],
---
+++ docs/.vitepress/theme/sidebar.ts
@@ -0,0 +1,24 @@
+export default [
+  {
+    text: "Getting Started",
+    link: "/guide"
+  },
+  {
+    text: "Email API",
+    collapsed: false,
+    items: [
+      { text: "Send", link: "/modules/emails/send" },
+      { text: "Check Domain", link: "/modules/emails/check-domain" },
+      {
+        text: "Webhooks",
+        collapsed: true,
+        items: [
+          { text: "Enroll for Webhook", link: "/modules/emails/webhooks#enroll-method" },
+          { text: "List Webhooks", link: "/modules/emails/webhooks#list-method" },
+          { text: "Delete Webhooks", link: "/modules/emails/webhooks#delete-method" },
+          { text: "Get Signing Key", link: "/modules/emails/webhooks#signing-key-method" }
+        ]
+      }
+    ]
+  }
+];
