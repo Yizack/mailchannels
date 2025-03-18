5e1ff4e9c3a529f51a71232f65714da7fdf71378
Yizack Rangel <yizackr@gmail.com>
2025-03-18 15:05:54 -0500

    chore: fix sub-accounts file name

--- src/modules.ts
+++ src/modules.ts
@@ -1,5 +1,5 @@
 export { Emails } from "./modules/emails";
 export { Webhooks } from "./modules/webhooks";
-export { SubAccounts } from "./modules/sub-account";
+export { SubAccounts } from "./modules/sub-accounts";
 
 export type * from "./types";
