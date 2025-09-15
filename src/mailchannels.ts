94ec4d8e1b9473f70a164d234a8e732f81377ed1
Yizack Rangel <yizackr@gmail.com>
2025-09-15 23:13:34 +0200

    chore: sort by modules

--- src/mailchannels.ts
+++ src/mailchannels.ts
@@ -5,14 +5,17 @@ export { MailChannelsClient };
 export type * from "./types";
 
 export class MailChannels extends MailChannelsClient {
+  // Modules: Email API
   readonly emails = new Emails(this);
   readonly webhooks = new Webhooks(this);
   readonly subAccounts = new SubAccounts(this);
-  readonly service = new Service(this);
-  readonly domains = new Domains(this);
-  readonly users = new Users(this);
-  readonly lists = new Lists(this);
   readonly metrics = new Metrics(this);
+
+  // Modules: Inbound API
+  readonly domains = new Domains(this);
+  readonly lists = new Lists(this);
+  readonly users = new Users(this);
+  readonly service = new Service(this);
 
   constructor (key: string) {
     super(key);
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,14 +1,14 @@
 // Modules: Email API
 export type * from "./emails";
+export type * from "./webhooks";
 export type * from "./sub-accounts";
 export type * from "./metrics";
-export type * from "./webhooks";
 
 // Modules: Inbound API
 export type * from "./domains";
+export type * from "./lists";
 export type * from "./users";
 export type * from "./service";
-export type * from "./lists";
 
 // Shared types
 export type * from "./success-response";
