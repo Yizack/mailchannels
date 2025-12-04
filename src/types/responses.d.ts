5647662203b1284f9e61824f724001ec85617865
Yizack Rangel <yizackr@gmail.com>
2025-12-04 05:22:19 -0500

    docs(types): add data and error description to responses

--- src/types/responses.d.ts
+++ src/types/responses.d.ts
@@ -3,10 +3,19 @@ export interface SuccessResponse {
    * Whether the operation was successful.
    */
   success: boolean;
+  /**
+   * Error message if the operation failed.
+   */
   error: string | null;
 }
 
 export interface DataResponse<T> {
+  /**
+   * The response data.
+   */
   data: T | null;
+  /**
+   * Error message if the operation failed.
+   */
   error: string | null;
 }
