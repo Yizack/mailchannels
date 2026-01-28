5d4d93f4e2e5f0b8331bc33427ef9ed6831ce075
Yizack Rangel <yizackr@gmail.com>
2026-01-28 01:49:43 -0500

    fix(types): fix `queuedAt` type in `EmailsSendAsyncResponse`

--- src/types/emails/send-async.d.ts
+++ src/types/emails/send-async.d.ts
@@ -4,7 +4,7 @@ export type EmailsSendAsyncResponse = DataResponse<{
   /**
    * ISO 8601 timestamp when the request was queued for processing.
    */
-  queuedAt: string[];
+  queuedAt: string;
   /**
    * Unique identifier for tracking this async request. Will be included in all webhook events for this request.
    */
