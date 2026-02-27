0db294d26007878dd71dd4756c6afaec8ceda19d
Yizack Rangel <yizackr@gmail.com>
2026-02-27 10:34:41 -0500

    test: run in isolated mode

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
+    isolate: false,
     root: fileURLToPath(new URL("./", import.meta.url)),
     coverage: {
       include: ["src"],
