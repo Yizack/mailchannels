f0a0cb0d6d897fe0c2c57a0db0ab8eba5b4cfbad
Yizack Rangel <yizackr@gmail.com>
2026-02-27 10:52:01 -0500

    docs(modules-source): add tests link to module source links

--- docs/.vitepress/plugins/utils/modules-source.ts
+++ docs/.vitepress/plugins/utils/modules-source.ts
@@ -7,9 +7,11 @@ export const addSourceLinks = (src: string, module: string, slug: string): strin
   let sourceUrl = `${URL}/src/modules/${module}.ts`;
   let playgroundUrl = `${URL}/playground/${module}`;
   let docsUrl = `${URL}/docs/modules/${module}/${slug}.md`;
+  let testsUrl = `${URL}/test/modules/${module}`;
 
   if (slug !== "index") {
     playgroundUrl += `/${slug}.ts`;
+    testsUrl += `/${slug}.test.ts`;
     const lineNumber = getMethodLineNumber(module, camelCase(slug));
     if (lineNumber) {
       sourceUrl += `#L${lineNumber}`;
@@ -19,7 +21,8 @@ export const addSourceLinks = (src: string, module: string, slug: string): strin
   const links = ([
     ["Source", sourceUrl],
     ["Playground", playgroundUrl],
-    ["Docs", docsUrl]
+    ["Docs", docsUrl],
+    ["Tests", testsUrl]
   ]).filter(i => i)
     .map(i => `[${i![0]}](${i![1]})`)
     .join(" • ");
