cf25477d6eb706ee665ea44b4b2d4693739badf4
Yizack Rangel <yizackr@gmail.com>
2026-04-06 20:40:44 -0500

    chore: remove duplicated type export

--- src/types/emails/index.ts
+++ src/types/emails/index.ts
@@ -3,6 +3,5 @@ export type * from "./send-async";
 export type * from "./check-domain";
 export type * from "./create-dkim-key";
 export type * from "./get-dkim-keys";
-export type * from "./rotate-dkim-key";
 export type * from "./update-dkim-key";
 export type * from "./rotate-dkim-key";
