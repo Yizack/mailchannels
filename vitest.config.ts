206cd9159740056696545dcef733df1f8c1a6a47
Yizack Rangel <yizackr@gmail.com>
2026-04-09 14:29:41 -0500

    test: remove isolate and server deps settings in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,13 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
-    isolate: false,
     root: fileURLToPath(new URL("./", import.meta.url)),
-    server: {
-      deps: {
-        inline: ["ofetch"]
-      }
-    },
     coverage: {
       include: ["src"],
       exclude: ["src/types"]
