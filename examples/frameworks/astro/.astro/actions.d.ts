b483371c450a9ae7617f7b7427b7c51bef262df5
Yizack Rangel <yizackr@gmail.com>
2026-05-05 19:08:56 -0500

    docs: remove autogenerated astro files

--- examples/frameworks/astro/.astro/actions.d.ts
+++
@@ -1,5 +0,0 @@
-import type * as ActionsModule from "E:/GitHub/mailchannels/examples/astro/src/actions/index.ts";
-
-declare module "astro:actions" {
-  export const actions: typeof ActionsModule["server"];
-}
